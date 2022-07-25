05b97daa1af22447e5cbc384ad486e1643d625ef
Chris Porter <cap8095@gmail.com>
2022-07-25 12:11:35 -0400

    adding reviews and an about me blurb

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,16 +1,54 @@
 import React from "react";
-// import ProductList from "../components/ProductList";
-import CategoryMenu from "../components/CategoryMenu";
+import styles from "../index.css";
+// import CategoryMenu from "../components/CategoryMenu";
 import Cart from "../components/Cart";
-
+import image from "../assets/bras.jpg";
 const About = () => {
   return (
-    <div className="container">
-      <CategoryMenu />
+    <div>
+      {/* <CategoryMenu /> */}
+      <img
+        src={image}
+        className="my-2"
+        style={{ width: "max-width", height: "30%" }}
+        alt="cover"
+      />
       <div>
-        <p>Welcome to Bistro Box! </p>
+        <h1>Welcome to Bistro Box!</h1>
+
+        <div>
+          {/* apply an orange border to the paragraph below */}
+          <h5>
+            <p className={styles.cell}>
+              It brings us great pleasure to present to you the easiest meal kit
+              ordering platform on the market.<br></br> We know that life can be
+              very busy, between work, school activities, kids sports, and
+              whatever curveball<br></br> that life throws at you, having time
+              to cook and prep a meal for the family can often times not be
+              feasible. <br></br>Bistro Box is here to lend you a helping hand,
+              while bringing you meals that will accomodate your specific
+              dietary restrictions.
+            </p>
+          </h5>
+        </div>
+        <p className="review">
+          "I'm going to be honest with you, before I started using BistroBox, my
+          family was eating a lot of frozen junk and fast food.<br></br>
+          Now that we've switched to using BistroBox several times a week, I can
+          finally feel proud of the food that I'm feeding our family."<br></br>-
+          Stephanie
+        </p>
+        <p className="review">
+          "I'm generally an anxious person, so the added stress of needing to
+          feed our family <br></br>
+          puts a lot of emotional burden on me, which I really needed to
+          alleviate. <br></br>
+          Ever since I've begun ordering BistroBox, it's as if a weight has been
+          lifted off of my shoulders." <br></br>- Jean-Louis
+        </p>
+        <p className="review">""</p>
       </div>
-      {/* <ProductList /> */}
+
       <Cart />
     </div>
   );
